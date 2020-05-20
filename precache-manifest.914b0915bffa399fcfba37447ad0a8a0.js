ab5b05003000f70abd235cedf04dee5726c4bf38
Nishantgoyal918 <nishantgoyal918@gmail.com>
2020-05-21 03:47:44 +0530

    Updates

---
+++ precache-manifest.914b0915bffa399fcfba37447ad0a8a0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca9c5e31c3ce0d11bf3d3a3c644e6ac",
+    "url": "/myWebsite/index.html"
+  },
+  {
+    "revision": "09c047af4c11bfb8fb33",
+    "url": "/myWebsite/static/css/main.fed0e8b9.chunk.css"
+  },
+  {
+    "revision": "25e5138c1e2b5f576976",
+    "url": "/myWebsite/static/js/2.02d01c4d.chunk.js"
+  },
+  {
+    "revision": "ab6bd92c46b489bf0abe330868a5708e",
+    "url": "/myWebsite/static/js/2.02d01c4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c047af4c11bfb8fb33",
+    "url": "/myWebsite/static/js/main.e91cd6d7.chunk.js"
+  },
+  {
+    "revision": "d9fee95ecd5f54eee882",
+    "url": "/myWebsite/static/js/runtime-main.afd30802.js"
+  },
+  {
+    "revision": "6ff11806aaf1066f8ea8c67fd8af842b",
+    "url": "/myWebsite/static/media/back-1.6ff11806.png"
+  },
+  {
+    "revision": "1e64326b9efb2083cfaeaf9265a96cfc",
+    "url": "/myWebsite/static/media/icofont.1e64326b.woff"
+  },
+  {
+    "revision": "36e0de11e7077cf29b29e6526468dd84",
+    "url": "/myWebsite/static/media/icofont.36e0de11.woff2"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myWebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
